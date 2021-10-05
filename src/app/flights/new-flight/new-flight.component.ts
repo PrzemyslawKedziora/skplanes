f77583dc1b173bfb6c1500a269941fe784de019f
Przemysław Kędziora <pkedziora347@gmail.com>
2021-10-05 22:59:00 +0200

    adding flights is now available

--- src/app/flights/new-flight/new-flight.component.ts
+++ src/app/flights/new-flight/new-flight.component.ts
@@ -1,6 +1,7 @@
 import {Component, ViewChild} from '@angular/core';
 import {MatDialogRef} from "@angular/material/dialog";
 import {FlightFormComponent} from "../flight-form/flight-form.component";
+import {FlightsService} from "../../core/services/flights.service";
 
 @Component({
   selector: 'app-new-flight',
@@ -8,14 +9,26 @@ import {FlightFormComponent} from "../flight-form/flight-form.component";
   styleUrls: ['./new-flight.component.css']
 })
 export class NewFlightComponent {
- // @ViewChild('FlightForm') flightForm?: FlightFormComponent;
+  @ViewChild('FlightForm') flightForm?: FlightFormComponent ;
 
   constructor(
+    private flightsService: FlightsService,
     private dialogRef: MatDialogRef<NewFlightComponent>
   ) { }
 
 
   createFlight() {
-    
+    console.log(this.flightForm);
+    this.flightsService.addFlight(this.flightForm?.form.value)
+      .then(this.onCreatingSuccess.bind(this), this.onCreatingFailure.bind(this))
   }
+
+  private onCreatingSuccess() {
+    this.dialogRef.close();
+  }
+
+  private onCreatingFailure() {
+    console.log('error')
+  }
+
 }
