e58099210ced80d7900681861405c919ca785da8
Przemysław Kędziora <pkedziora347@gmail.com>
2021-12-05 01:59:02 +0100

    Showing logged user

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,25 +1,23 @@
-import {NgModule} from "@angular/core";
-import {CommonModule} from "@angular/common";
-import {LoginComponent} from "./login/login.component";
-import {DashboardComponent} from "./dashboard/dashboard.component";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
 import {RouterModule} from "@angular/router";
-import {MatIconModule} from "@angular/material/icon";
-import {MatToolbarModule} from "@angular/material/toolbar";
 import {MaterialModule} from "../material/material.module";
 import {FormsModule} from "@angular/forms";
 
 
+
 @NgModule({
+  declarations: [
+    DashboardComponent,
+    LoginComponent
+  ],
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule,
-    MatIconModule,
-    MatToolbarModule,
-    MaterialModule,
-    FormsModule
-  ],
-  declarations: [DashboardComponent, LoginComponent],
-  exports: [DashboardComponent, LoginComponent],
-  }
-)
-  export class CoreModule { }
+    MaterialModule
+  ]
+})
+export class CoreModule { }
--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from "@angular/core";
 import {AngularFireAuth} from "@angular/fire/auth";
-import firebase from 'firebase';
+import firebase from "firebase";
 import UserInfo = firebase.UserInfo;
 
 @Injectable({providedIn: 'root'})
@@ -8,19 +8,25 @@ export class AuthService {
   private userData?: firebase.User | null;
 
 
-  constructor(private fireAuth: AngularFireAuth) {
-  }
+  constructor(private fireAuth: AngularFireAuth) {}
 
-  login(credentials: { email: string, password: string}) {
+
+  login(credentials: {email: string, password: string}) {
     return this.fireAuth.signInWithEmailAndPassword(credentials.email, credentials.password)
-      .then( user => this.userData = user.user)
-  }
-  register(credentials: {email: string, password: string}) {
-    return this.fireAuth.createUserWithEmailAndPassword(credentials.email, credentials.password);
+      .then(user => this.userData = user.user);
   }
 
   get user() {
     return this.userData;
   }
-}
 
+
+  register(credentials: {email: string, password: string}) {
+    return this.fireAuth.createUserWithEmailAndPassword(credentials.email, credentials.password);
+  }
+
+  logout() {
+    return this.fireAuth.signOut();
+  }
+
+}
